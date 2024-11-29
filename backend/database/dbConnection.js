a49ba28f4e3970944994c93bd7a2e7d285fb0b54
Shubham Prajapati <138375156+Tajmaha8849@users.noreply.github.com>
2024-11-29 22:52:01 +0530

    Update dbConnection.js

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -1,11 +1,24 @@
 import mongoose from "mongoose";
 
 export const dbConnection=()=>{
-    mongoose.connect(process.env.MONGO_URL,{
-        dbName:"MERN_STACK_JOB_SEEKING",
+    mongoose.connect("mongodb+srv://Shubham12P:Shubham12P@cluster0.chy56.mongodb.net/",{
+        dbName:"JobPortal",
     }).then(()=>{
         console.log("Connected to database.")
     }).catch(()=>{
         console.log("Some error ocurred.")
     })
 }
+
+// export const dbConnection=()=>mongoose.connect("mongodb+srv://shubhamprajapati9537:9265556597@cluster0.mongodb.net/JobPortal", {
+//     dbName: "M",
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     retryWrites: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to database.");
+//   })
+//   .catch((error) => {
+//     console.error("Database connection error:", error.message);
+//   });
